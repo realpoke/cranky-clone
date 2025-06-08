c909f12ea9037eadaf6fff7d5075f8c4b81925d4
RealPoke <m@rtin.win>
2025-06-08 06:25:38 +0200

    chore: Added parallel testing

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn() => view('welcome'))->name('home');
+Route::get('/', fn () => view('welcome'))->name('home');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
