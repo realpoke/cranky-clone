06307fed1a22b229da23468ab633bedbff472f47
RealPoke <m@rtin.win>
2025-06-08 22:49:14 +0200

    chore: Updated some Laravel defaults

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,13 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +26,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::unguard();
+        Model::shouldBeStrict();
+        Model::automaticallyEagerLoadRelationships();
+
+        DB::prohibitDestructiveCommands(
+            App::isProduction()
+        );
+
+        Http::preventStrayRequests(
+            App::runningUnitTests()
+        );
+
+        Date::use(
+            CarbonImmutable::class
+        );
+
+        URL::forceHttps(
+            App::isProduction()
+        );
     }
 }
